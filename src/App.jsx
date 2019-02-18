9310db4a4341fbfb28372f6804f7ec003ee85d42
Morris Liu <morrislew@outlook.com>
2019-02-19 00:06:10 +0800

    Initialize Koa back-end

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <code>lightmerge</code>
+    </header>
+  </div>
+);
+
+export default App;
