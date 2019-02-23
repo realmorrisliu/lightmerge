89c2ae0f93c6888959c3caba2f61bffd887d0cd3
Morris Liu <morrislew@outlook.com>
2019-02-23 11:40:40 +0800

    feature: lightmerge button developing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import StatusViewer from './components/StatusViewer';
 import styles from './App.module.scss';
 
 const branches = ['master', 'lightmerge', 'lym/chatroom', 'lym/group-chat', 'lym/video', 'feature/chatroom', 'feature/messagebox'];
-const originSelectedBranches = ['lym/chatroom', 'feature/chatroom'];
+const originSelectedBranches = ['lym/chatroom', 'lym/group-chat'];
 
 const App = () => {
   const [selectedBranches, setSelectedBranches] = useState(originSelectedBranches);
@@ -18,12 +18,31 @@ const App = () => {
     }
   };
 
+  const isLightmergeAvailable = () => {
+    if (selectedBranches.length === 0) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const runLightmerge = () => {
+    console.log(selectedBranches);
+  };
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>user</header>
       <div className={styles.RepoPath}>
         <input className={styles.Input} type="text" placeholder="Repository Path" />
-        <button type="button" className={styles.Button} disabled>lightmerge</button>
+        <button
+          type="button"
+          className={styles.Button}
+          disabled={!isLightmergeAvailable()}
+          onClick={runLightmerge}
+        >
+          lightmerge
+        </button>
         <button type="button" className={styles.Button} disabled>deploy</button>
       </div>
       <div className={styles.Content}>
