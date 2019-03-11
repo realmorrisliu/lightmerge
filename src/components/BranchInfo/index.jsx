0c078fcd11abd4992e8b268285175d33627971b8
Morris Liu <morrislew@outlook.com>
2019-03-11 18:54:43 +0800

    mobx refactor ok

--- src/components/BranchInfo/index.jsx
+++ src/components/BranchInfo/index.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
 import BranchBlock from './BranchBlock';
 import styles from './BranchInfo.module.scss';
 import store from '../../stores/RootStore';
 
-const BranchInfo = ({ branchList, selectedBranchList }) => (
-  <div className={styles.BranchInfo}>
-    <div className={styles.Base}>
-      <span className={styles.Type}>Base branch:</span>
-      <select className={styles.BaseBranch} onChange={store.repo.updateBase}>
-        {
-          branchList.map(branch => <option key={branch} value={branch}>{branch}</option>)
-        }
-      </select>
-    </div>
-    <div className={styles.Source}>
-      <span className={styles.Type}>Source branch:</span>
-      <span className={styles.Blocks}>
-        {selectedBranchList.map(branch => <BranchBlock key={branch} name={branch} />)}
-      </span>
-    </div>
-    <div className={styles.Target}>
-      <span className={styles.Type}>Target branch:</span>
-      <span className={styles.Blocks}>
-        <BranchBlock name="lightmerge" />
-      </span>
-    </div>
-  </div>
-);
+@observer
+class BranchInfo extends React.Component {
+  render() {
+    const { repo } = store;
 
-BranchInfo.propTypes = {
-  branchList: PropTypes.arrayOf(PropTypes.string).isRequired,
-  selectedBranchList: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
+    return (
+      <div className={styles.BranchInfo}>
+        <div className={styles.Base}>
+          <span className={styles.Type}>Base branch:</span>
+          <select className={styles.BaseBranch} onChange={store.repo.updateBase}>
+            {
+              repo.branches.map(branch => <option key={branch} value={branch}>{branch}</option>)
+            }
+          </select>
+        </div>
+        <div className={styles.Source}>
+          <span className={styles.Type}>Source branch:</span>
+          <span className={styles.Blocks}>
+            {
+              repo.selectedBranches.map(branch => <BranchBlock key={branch} name={branch} />)
+            }
+          </span>
+        </div>
+        <div className={styles.Target}>
+          <span className={styles.Type}>Target branch:</span>
+          <span className={styles.Blocks}>
+            <BranchBlock name="lightmerge" />
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default BranchInfo;
